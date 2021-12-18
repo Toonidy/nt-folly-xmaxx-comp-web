34a65871ecd6143826ae0347649478b253584891
Yong Tan <ultimatoonmaster@gmail.com>
2021-12-18 20:55:24 +0800

    Fix react warning

--- src/components/CompetitionResult/index.tsx
+++ src/components/CompetitionResult/index.tsx
@@ -551,13 +551,13 @@ const getMultiplierBackgroundColor = (m: number) => {
 const getRankText = (i: number) => {
 	var j = i % 10,
 		k = i % 100
-	if (j == 1 && k != 11) {
+	if (j === 1 && k !== 11) {
 		return i + "st"
 	}
-	if (j == 2 && k != 12) {
+	if (j === 2 && k !== 12) {
 		return i + "nd"
 	}
-	if (j == 3 && k != 13) {
+	if (j === 3 && k !== 13) {
 		return i + "rd"
 	}
 	return i + "th"
