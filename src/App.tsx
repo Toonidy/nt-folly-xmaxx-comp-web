bd66bd670de522c6db0759a36416d0b82df75f8a
Yong Tan <ultimatoonmaster@gmail.com>
2021-12-21 01:00:24 +0800

    Adding some more tips

--- src/App.tsx
+++ src/App.tsx
@@ -123,8 +123,8 @@ export const App = () => {
 										Don't have time to race? Just do <em>one</em> and try to win the accuracy reward at any <em>blitz timeslot</em>.
 									</Typography>
 									<Typography component={"li"}>
-										If you plan to a certain amount of races in one session, always do the last race on the Blitz Time. That way you can
-										enter <code>Speed</code> and <code>Accuracy</code> blitz.
+										If you plan to a certain amount of races in one session, always do the last race on the next Blitz Time. That way you
+										can enter <code>Speed</code> and <code>Accuracy</code> blitz.
 									</Typography>
 									<Typography component={"li"}>
 										Blitzes technically starts and ends at every X:X1; the scheduled times displayed are for easy readings. For example:{" "}
